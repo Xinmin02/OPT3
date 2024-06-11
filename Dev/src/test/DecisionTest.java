4375b3e3ff0bb982d71ff493700ca69d2d24bf58
Xinmin02 <151744528+Xinmin02@users.noreply.github.com>
2024-06-11 22:43:54 +0200

    test

---
+++ Dev/src/test/DecisionTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DecisionTest {
+
+    public boolean decision(boolean A, boolean B, boolean C) {
+        return A && B && C;
+    }
+
+    @Test
+    public void testDecision() {
+
+        assertTrue(decision(true, true, true));
+
+
+        assertFalse(decision(true, true, false));
+
+        assertFalse(decision(true, false, true));
+
+
+        assertFalse(decision(true, false, false));
+
+
+        assertFalse(decision(false, true, true));
+
+
+        assertFalse(decision(false, true, false));
+
+
+        assertFalse(decision(false, false, true));
+
+
+        assertFalse(decision(false, false, false));
+    }
+}
